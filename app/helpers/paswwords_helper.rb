cc6fa48c8efcb5a310a3228e8ececf8a8e06cbe9
EC2 Default User <ec2-user@ip-172-31-59-106.ec2.internal>
2020-12-18 12:26:22 +0000

    controller-public

---
+++ app/helpers/paswwords_helper.rb
@@ -0,0 +1,2 @@
+module PaswwordsHelper
+end
---
+++ test/controllers/paswwords_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PaswwordsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
